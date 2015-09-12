231db27dfb43616d4671c7508e664501d1789637
Vitaliy Stepanenko <mail@vitaliy.in>
2015-09-12 18:16:01 +0300

    upd

--- app/Http/Controllers/Admin/GroupsController.php
+++ app/Http/Controllers/Admin/GroupsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Group;
+use App\Models\User;
+use Nayjest\Grids\DataRow;
+use Nayjest\Grids\FieldConfig;
 use Nayjest\Grids\Grids;
 
 class GroupsController extends Controller
@@ -12,8 +15,58 @@ class GroupsController extends Controller
     {
 
         $grid = Grids::make([
-            'src' => Group::class
+            'src' => Group::class,
+            'columns' => [
+                'id',
+                'name',
+                [
+                    'name' => 'actions',
+                    'label' => 'Actions',
+                    'callback' => function($value, DataRow $row){
+                        $manageMembersUrl = '/admin/groups/members/' . $row->getCellValue('id');
+                        $manageMembersLink = "<a class='btn btn-info' href='$manageMembersUrl'>Manage Members</a>";
+
+                        $manageMembersUrl = '/admin/groups/delete/' . $row->getCellValue('id');
+                        $deleteGroupLink = "<a class='btn btn-danger' href='$manageMembersUrl'>Delete</a>";
+
+                        return "<div class='btn-group'>$manageMembersLink $deleteGroupLink</div>";
+                    }
+                ]
+            ]
         ])->render();
-        return view('admin.default.grid-page', compact('grid'));
+        $title = 'Groups Management';
+        $links = [
+            'groups/create' => 'New Group',
+        ];
+        return view('admin.default.grid-page', compact('grid', 'title'));
+    }
+
+    public function getMembers($groupId)
+    {
+        $group = Group::findOrFail($groupId);
+        $users = (new User)->newQuery()
+            ->leftJoin('user_groups', 'users.id','=','user_groups.user_id')
+            ->where('user_groups.group_id', '=', $groupId);
+
+        $grid = Grids::make([
+            'src' => $users,
+            'columns' => [
+                'id',
+                'email',
+                'first_name',
+                'last_name',
+                'department',
+                'position',
+                [
+                    'name' => 'actions',
+                    'label' => 'Actions',
+                    'callback' => function(){
+                        return "<a class='btn btn-info'>Remove</a>";
+                    }
+                ]
+            ]
+        ])->render();
+        $title = "[$group->name] Group Management";
+        return view('admin.default.grid-page', compact('grid', 'title'));
     }
 }
--- resources/views/admin/default/grid-page.blade.php
+++ resources/views/admin/default/grid-page.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.backend')
 
 @section('content')
-    <?php
-    if (isset($title)) {
-        echo "<h1>$title</h1><hr>";
-    }
-    ?>
-    <?= $grid ?>
+    @if(isset($title))
+        <h1>{!! $title !!}</h1><hr>
+    @endif
+    @if(isset($links))
+        @foreach($links as $url => $label)
+            <a class="btn btn-info" href="{!! $url !!}">{!! $label !!}</a>
+        @endforeach
+        <br>
+    @endif
+
+    {!! $grid !!}
 @stop
