a4563ed49cbb632c43970e40e56deadc14baec4a
Vitalii Ofat <v.ofat@globo-tech.com>
2015-09-12 18:16:30 +0300

    update process

---
+++ public/admin/js/survey.form.js
@@ -0,0 +1,27 @@
+!function($){
+    var questionTpl = doT.template( $("#questionTemplate").html()),
+        answerTpl = doT.template( $("#answersTemplate").html()),
+
+        questionIterator = $(".question").length;
+
+    $("#addQuestion").click(function(e){
+        e.preventDefault();
+
+        $("#questionsList").append(questionTpl({id: questionIterator}));
+        questionIterator++;
+    });
+
+    $(".addAnswer").click(function(e){
+        e.preventDefault();
+
+        var question = $(this).closest('.question'),
+            questionId = question.data('id'),
+            answerIterator = question.find('.answer').length,
+            html = answerTpl({
+                question: questionId,
+                id: answerIterator
+            });
+
+        question.find('.answersList').append(html);
+    });
+}(jQuery);
--- resources/views/admin/survey/form.blade.php
+++ resources/views/admin/survey/form.blade.php
@@ -1,3 +1,7 @@
+<?php
+View::addExtension('dot','php');
+?>
+
 @extends('layouts.backend')
 
 @section('title')
@@ -5,69 +9,92 @@
 @stop
 
 @section('content')
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
-        <h3 class="page-header">Create Survey</h3>
-        <?=Form::open(['route' => 'admin.survey.save', 'method' => 'post'])?>
-        <div class="form-group">
-            <label for="inputTitle">Title</label>
-            <?=Form::text('survey[title]', $model->title, ['class' => 'form-control', 'id' => 'inputTitle'])?>
-        </div>
-        <div class="form-group">
-            <label for="inputDescription">Description</label>
-            <?=Form::textarea('survey[description]', $model->description, ['class' => 'form-control', 'id' => 'inputDescription', 'rows' => 5])?>
-        </div>
-        <div class="form-group">
-            <label for="inputMailDescription">Mail Description <small>(optional)</small></label>
-            <?=Form::textarea('survey[mail_description]', $model->mail_description, ['class' => 'form-control', 'id' => 'inputMailDescription', 'rows' => 5])?>
-            <div class="help-block">
-                Describe your survey and send it by e-mail
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h3 class="page-header">Create Survey</h3>
+            <?=Form::open(['route' => 'admin.survey.save', 'method' => 'post'])?>
+            <div class="form-group">
+                <label for="inputTitle">Title</label>
+                <?=Form::text('survey[title]', $model->title, ['class' => 'form-control', 'id' => 'inputTitle'])?>
             </div>
-        </div>
-        <div class="checkbox">
-            <label for="inputIsAnon">
-                <?=Form::checkbox('survey[is_anon]', $model->is_anon, false)?>
-                Is Anonymous
-            </label>
-        </div>
-        <h4 class="page-header">Questions</h4>
-        <?php foreach($questions as $k=>$question):?>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <a href="#" class="close">&times;</a>
-                <div class="form-group">
-                    <label>Title</label>
-                    <?=Form::text('question['.$k.'][title]', $question->title, ['class' => 'form-control'])?>
-                </div>
-                <div class="form-group">
-                    <label>Description</label>
-                    <?=Form::textarea('question['.$k.'][description]', $question->description, ['class' => 'form-control', 'rows' => 2])?>
-                </div>
-                <div class="form-group">
-                    <label>Type</label>
-                    <?=Form::select('question['.$k.'][type]', [
-                            \App\Models\Question::TYPE_SIMPLE => 'One Answer',
-                            \App\Models\Question::TYPE_MULTIPLE => 'Multiple Answers',
-                            \App\Models\Question::TYPE_ORDER => 'Reorder Answers',
-                    ], $question->type, ['class' => 'form-control'])?>
-                </div>
-                <h5 class="page-header">
-                    Answers
-                </h5>
-                <?php foreach($question->answers as $i=>$answer):?>
-                <div class="input-group">
-                    <?=Form::text('question['.$k.'][answer]['.$i.'][text]', $question->text, ['class' => 'form-control'])?>
-                    <span class="input-group-btn">
-                        <a href="#" class="btn btn-warning">&times;</a>
-                    </span>
-                </div>
-                <?php endforeach ?>
+            <div class="form-group">
+                <label for="inputDescription">Description</label>
+                <?=Form::textarea('survey[description]', $model->description, ['class' => 'form-control', 'id' => 'inputDescription', 'rows' => 5])?>
             </div>
+            <div class="form-group">
+                <label for="inputMailDescription">Mail Description
+                    <small>(optional)</small>
+                </label>
+                <?=Form::textarea('survey[mail_description]', $model->mail_description, ['class' => 'form-control', 'id' => 'inputMailDescription', 'rows' => 5])?>
+                <div class="help-block">
+                    Describe your survey and send it by e-mail
+                </div>
+            </div>
+            <div class="checkbox">
+                <label for="inputIsAnon">
+                    <?=Form::checkbox('survey[is_anon]', $model->is_anon, false)?>
+                    Is Anonymous
+                </label>
+            </div>
+            <h4 class="page-header">Questions</h4>
+            <div id="questionsList">
+            <?php foreach($questions as $k=>$question):?>
+            <div class="panel panel-default question" data-id="<?=$k?>">
+                <div class="panel-body">
+                    <a href="#" class="close">&times;</a>
+
+                    <div class="form-group">
+                        <label>Title</label>
+                        <?=Form::text('question[' . $k . '][title]', $question->title, ['class' => 'form-control'])?>
+                    </div>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <?=Form::textarea('question[' . $k . '][description]', $question->description, ['class' => 'form-control', 'rows' => 2])?>
+                    </div>
+                    <div class="form-group">
+                        <label>Type</label>
+                        <?=Form::select('question[' . $k . '][type]', [
+                                \App\Models\Question::TYPE_SIMPLE => 'One Answer',
+                                \App\Models\Question::TYPE_MULTIPLE => 'Multiple Answers',
+                                \App\Models\Question::TYPE_ORDER => 'Reorder Answers',
+                        ], $question->type, ['class' => 'form-control'])?>
+                    </div>
+                    <h5 class="page-header">
+                        Answers
+                    </h5>
+                    <div class="answersList">
+                    <?php foreach($question->answers as $i=>$answer):?>
+                    <div class="form-group answer">
+                        <div class="input-group">
+                            <?=Form::text('question[' . $k . '][answer][' . $i . '][text]', $question->text, ['class' => 'form-control'])?>
+                            <span class="input-group-btn">
+                                <a href="#" class="btn btn-warning">&times;</a>
+                            </span>
+                        </div>
+                    </div>
+                    <?php endforeach ?>
+                    </div>
+                    <hr>
+                    <a href="#" class="btn btn-sm btn-info addAnswer">Add Answer</a>
+                </div>
+            </div>
+            <?php endforeach ?>
+            </div>
+            <a href="#" class="btn btn-sm btn-info" id="addQuestion">Add Question</a>
+            <hr>
+            <button type="submit" class="btn btn-primary">Save</button>
+            <?=Form::close()?>
         </div>
-        <?php endforeach ?>
-        <hr>
-        <button type="submit" class="btn btn-primary">Save</button>
-        <?=Form::close()?>
     </div>
-</div>
+@stop
+
+@section('scripts')
+<script type="text/x-dot-template" id="questionTemplate">
+    @include('admin.survey.question')
+</script>
+<script type="text/x-dot-template" id="answersTemplate">
+    @include('admin.survey.answer')
+</script>
+<script src="/admin/js/doT.min.js"></script>
+<script src="/admin/js/survey.form.js"></script>
 @stop
